88a260ee02a018742069f54a0c923ae91e46a2d8
soiferj <soiferj@umich.edu>
2016-04-07 17:13:52 -0400

    Read piano circuits and send to serial out as byte-long state

--- Main/Main.ino
+++ Main/Main.ino
@@ -3,11 +3,12 @@ const int OctavePins[] = { 2, 3, 4, 5, 6, 7, 8 };
 
 void setup()
 {
-  Serial.begin(689);
+  Serial.begin(9600);
   for (int i = 0; i < NumOctaves; i++)
   {
     pinMode(OctavePins[i], INPUT);
   }
+  pinMode(13, OUTPUT);
 }
 
 void loop()
@@ -19,10 +20,24 @@ void loop()
   for (int i = 0; i < 7; i++)
   {
     val = digitalRead(OctavePins[i]);
-    pianoInfo |= val << (NumOctaves - i - 1);
+    if (val == LOW)
+    {
+      pianoInfo |= 1 << (NumOctaves - i - 1);
+    }
   }
 
-  Serial.write(pianoInfo);  
+  if (pianoInfo == 0)
+  {
+    digitalWrite(13, LOW);
+  }
+  else
+  {
+    digitalWrite(13, HIGH);
+  }
+
+  Serial.println(pianoInfo, BIN);
+
+  delay(100);
 
   // read info from pi
 
